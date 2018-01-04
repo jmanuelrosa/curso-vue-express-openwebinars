bb1948998547f25426383bf6157aa375f5b323a3
José Manuel Rosa <josemanuel.rosamoncayo@gmail.com>
2018-01-04 19:51:53 +0100

    Add config

---
+++ server/src/config.js
@@ -0,0 +1,23 @@
+import bodyParser from 'body-parser'
+import logger from 'morgan'
+import cors from 'cors'
+import { config } from 'dotenv'
+
+const SETTINGS = config()
+
+export default app => {
+  app.disable('x-powered-by')
+
+  app.set('env', SETTINGS.parsed.ENV)
+
+  app.set('config', SETTINGS.parsed)
+  app.locals.env = app.get('env')
+  app.locals.config = app.get('config')
+
+  app.use(logger('combined'))
+
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: false }))
+
+  app.use(cors())
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,14 @@
 import express from 'express'
 
+import config from './config'
+
 let _server
 
 const server = {
   start() {
     const app = express()
+
+    config(app)
 
     _server = app.listen('9000', () => {
       const address = _server.address()
